65adfc6f4b92b13efc37fb9c1dfd9e3bd2926b01
ねこちゃん <wildenyankoman@gmail.com>
2024-10-08 08:05:25 +0900

    test

--- .github/actions/my-action2/main.js
+++ .github/actions/my-action2/main.js
@@ -2,3 +2,4 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 core.info(JSON.stringify(github.context.payload.pull_request?.base, null, 2));
+core.exportVariable('XXXXX', 'yyyyy');
